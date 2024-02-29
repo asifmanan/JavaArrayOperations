84d28926ea79ce919f60a9755bfb327b61f859f5
asifmanan <asifmanan@gmail.com>
2024-02-29 21:12:26 +0000

    Runner class and main method for testing added

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Runner {
+public class Runner {
+    public static void main(String[] args) {
+        IntArray arr = new IntArray(2);
+//        arr.add(1);
+        arr.add(2);
+        System.out.println(arr.get(0));
+        System.out.println(arr.get(1));
+    }
 }
