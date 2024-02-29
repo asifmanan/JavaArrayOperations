fb12564d30e89b3c540c541f6ea410156471cd2b
asifmanan <asifmanan@gmail.com>
2024-02-29 22:37:34 +0000

    Wrote and executed test method for find()

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,6 @@
 public class Runner {
     public static void main(String[] args) {
-        removeMethodTest();
+        findMethodTest();
     }
 
     public static void addMethodTest(){
@@ -21,6 +21,14 @@ public class Runner {
         System.out.println(arr.get(1));
         arr.removeIndex(1);
         System.out.println(arr.get(1));
+    }
+    public static void findMethodTest(){
+        IntArray arr = new IntArray(3);
+        arr.add(0);
+        arr.add(1);
+        arr.add(2);
+        System.out.println("Item at index(2): " + arr.get(2));
+        System.out.println("Found at index: " + arr.find(2));
     }
 
 
