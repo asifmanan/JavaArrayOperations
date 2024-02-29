bdee3cfcbee8b1b8aaec1ae4a2af0f1f56a0f800
asifmanan <asifmanan@gmail.com>
2024-02-29 22:18:57 +0000

    removeIndex() test method added in runner and working

--- src/Runner.java
+++ src/Runner.java
@@ -1,9 +1,27 @@
 public class Runner {
     public static void main(String[] args) {
+        removeMethodTest();
+    }
+
+    public static void addMethodTest(){
         IntArray arr = new IntArray(2);
 //        arr.add(1);
         arr.add(2);
         System.out.println(arr.get(0));
         System.out.println(arr.get(1));
     }
+
+    public static void removeMethodTest(){
+        IntArray arr = new IntArray(5);
+        arr.add(0);
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        System.out.println(arr.get(1));
+        arr.removeIndex(1);
+        System.out.println(arr.get(1));
+    }
+
+
 }
